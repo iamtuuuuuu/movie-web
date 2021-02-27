86b3b3c3da048a9d2da7a55579f64d062d1edf24
iamtuuuuuu <vuanhtu111127@gmail.com>
2021-02-27 17:10:59 +0700

    chua respon dc slider

---
+++ js/script.js
@@ -0,0 +1,75 @@
+// $(document).ready(function() {
+//     $('#autoWidth').lightSlider({
+//         item:5,
+//         loop:true,
+//         slideMove:1,
+//         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+//         speed:600,
+//         responsive : [
+//             {
+//                 breakpoint:1600,
+//                 settings: {
+//                     item:5,
+//                     slideMove:1,
+//                     slideMargin:6,
+//                   }
+//             },
+//             {
+//                 breakpoint:1024,
+//                 settings: {
+//                     item:3,
+//                     slideMove:1,
+//                     slideMargin:6,
+//                   }
+//             },
+//             {
+//                 breakpoint:739,
+//                 settings: {
+//                     item:2,
+//                     slideMove:1
+//                   }
+//             }
+//         ]
+//     });  
+//   });
+
+$(document).ready(function() {
+  
+    $(".movie-wrapper").slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      loop: true,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      nextArrow: $(".next"),
+      prevArrow: $(".prev"),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        // {
+        //   breakpoint: 480,
+        //   settings: {
+        //     slidesToShow: 1,
+        //     slidesToScroll: 1
+        //   }
+        // }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  });
